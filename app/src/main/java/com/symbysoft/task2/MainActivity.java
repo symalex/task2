ded21d84626f6a2d3b6476eb11a984213c85d20e
Isaev A.V <symalex@gmail.com>
2015-11-09 01:10:00 +0600

    add MainActivity

---
+++ app/src/main/java/com/symbysoft/task2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.symbysoft.task2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_main);
+	}
+}
